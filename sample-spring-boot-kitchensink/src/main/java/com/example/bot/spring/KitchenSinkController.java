cff88d176d5bb7e1273ed55b33eeba7b1a344dea
G <G>
2020-07-15 04:01:57 +0100

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -329,12 +329,13 @@ public class KitchenSinkController {
                                         this.replyText(replyToken, throwable.getMessage());
                                         return;
                                     }
+                                    this.replyText(replyToken, "ID: " + userId);
                                     this.reply(
                                             replyToken,
                                             Arrays.asList(new TextMessage("(from group)"),
-                                                          new TextMessage("ID: " + userId),
-                                                                  "Display name: " + profile.getDisplayName()),
                                                           new TextMessage(
+                                                                  "Display name: " + profile.getDisplayName()),
+                                                          new TextMessage("ID: " + userId),
                                                           new ImageMessage(profile.getPictureUrl(),
                                                                            profile.getPictureUrl()))
                                     );
