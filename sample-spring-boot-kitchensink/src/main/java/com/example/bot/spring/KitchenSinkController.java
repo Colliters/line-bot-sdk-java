c845a2bb819005a6e7c3b51e1a3acd774f759010
G <G>
2020-07-15 03:27:35 +0100

    test

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/KitchenSinkController.java
@@ -335,7 +335,7 @@ public class KitchenSinkController {
                                             Arrays.asList(new TextMessage("(from group)"),
                                                           new TextMessage(
                                                                   "Display name: " + profile.getDisplayName()),
-                                                          new TextMessage("ID: " + userId),
+                                            //              new TextMessage("ID: " + userId),
                                                           new ImageMessage(profile.getPictureUrl(),
                                                                            profile.getPictureUrl()))
                                     );
